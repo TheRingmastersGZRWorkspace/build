BlissRoms/platform_build_make

product: Remove some aosp apps
Change-Id: Ia394b85976ec74387d4d18c3483d36c95c06bc97
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

target/product/gsi_common.mk
@@ -75,6 +75,7 @@ PRODUCT_COPY_FILES += \
 # Support addtional P vendor interface
 PRODUCT_EXTRA_VNDK_VERSIONS := 28
 
+ifeq ($(BLISS_BUILD),)
 # More AOSP packages
 PRODUCT_PACKAGES += \
     messaging \
@@ -84,7 +85,6 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
-ifeq ($(BLISS_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 endif

TheRingmastersGZRWorkspace/build

target/product/gsi_common.mk
@@ -80,6 +80,7 @@ PRODUCT_COPY_FILES += \
 # Support addtional P vendor interface
 PRODUCT_EXTRA_VNDK_VERSIONS := 28
 
+ifeq ($(VALIDUS_BUILD),)
 # More AOSP packages
 PRODUCT_PACKAGES += \
     messaging \
@@ -89,7 +90,6 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
-ifeq ($(VALIDUS_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 endif