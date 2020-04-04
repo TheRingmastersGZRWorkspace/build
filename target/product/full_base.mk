BlissRoms/platform_build_make

product: Exclude all AOSP default tones and audio packages
- They override vendors props :/

Change-Id: Idc54bca63336ca3c0c2aa7ed7af75a972380ae10
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

target/product/full_base.mk
@@ -41,17 +41,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+ifeq ($(BLISS_BUILD),)
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
-
+endif
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
+ifeq ($(BLISS_BUILD),)
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-
+endif
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 

target/product/gsi_common.mk
@@ -20,13 +20,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+ifeq ($(BLISS_BUILD),)
 # Default AOSP sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg \
+    ro.config.notification_sound=pixiedust.ogg
+endif
 
 # The mainline checking whitelist, should be clean up
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_WHITELIST += \

target/product/handheld_system.mk
@@ -85,6 +85,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carrier=unknown \
+    ro.carrier=unknown
+    
+ifeq ($(BLISS_BUILD),)
+ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=OnTheHunt.ogg \
     ro.config.alarm_alert=Alarm_Classic.ogg
+endif

target/product/mainline.mk
@@ -24,6 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+ifeq ($(BLISS_BUILD),)
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -34,6 +35,5 @@ PRODUCT_PACKAGES += \
     PhotoTable \
     WallpaperPicker \
 
-ifeq ($(BLISS_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 endif

TheRingmastersGZRWorkspace/build

target/product/full_base.mk
@@ -41,17 +41,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+ifeq ($(VALIDUS_BUILD),)
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
-
+endif
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
+ifeq ($(VALIDUS_BUILD),)
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-
+endif
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 

target/product/gsi_common.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+ifeq ($(VALIDUS_BUILD),)
 # Default AOSP sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
@@ -31,7 +32,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg \
+    ro.config.notification_sound=pixiedust.ogg
+endif
 
 # The mainline checking whitelist, should be clean up
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_WHITELIST += \

target/product/handheld_system.mk
@@ -85,6 +85,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carrier=unknown \
+    ro.carrier=unknown
+    
+ifeq ($(VALIDUS_BUILD),)
+ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=OnTheHunt.ogg \
     ro.config.alarm_alert=Alarm_Classic.ogg
+endif

target/product/mainline.mk
@@ -24,6 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+ifeq ($(VALIDUS_BUILD),)
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -34,6 +35,5 @@ PRODUCT_PACKAGES += \
     PhotoTable \
     WallpaperPicker \
 
-ifeq ($(VALIDUS_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 endif